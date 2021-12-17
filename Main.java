2715d3bc43b24086fcce29ef82c278cc27dc11b5
p10r10 <asantiago@keemail.me>
2021-12-17 18:57:55 +0000

    exercises

--- Main.java
+++ Main.java
@@ -1,10 +1,24 @@
 import points.Point;
 
 class Main{
+
+    static boolean isLower(char c){
+        return c >= 'a' && c <= 'z';
+    }
+
+    static int countLower(String s) {
+        int count = 0, i = 0;
+        while (i < s.length())
+            if (isLower(s.charAt(i++)))
+                count++;
+        return  count;
+    }
+
     public static void main(String[] args){
         Point myPoint = new Point(10, 15);
         System.out.println(myPoint.getX());
-        System.out.println(myPoint.getY());
+        System.out.println(isLower('y'));
+        System.out.println(countLower("a"));
 
     }
 }
