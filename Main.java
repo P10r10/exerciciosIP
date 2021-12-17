dada154defc0202cd5344af1fa3e37ef43b6cf90
p10r10 <asantiago@keemail.me>
2021-12-17 00:08:34 +0000

    exercises

---
+++ Main.java
@@ -0,0 +1,10 @@
+import points.Point;
+
+class Main{
+    public static void main(String[] args){
+        Point myPoint = new Point(10, 15);
+        System.out.println(myPoint.getX());
+        System.out.println(myPoint.getY());
+
+    }
+}
